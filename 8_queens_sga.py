72d69624d776f247400f1ad0aa3905dbbafbc289
Reynaldo Salazar <reysalazar1213@gmail.com>
2020-11-18 21:15:33 -0600

    fitness bug causes invalid quantities of queens

--- 8_queens_sga.py
+++ 8_queens_sga.py
@@ -8,12 +8,13 @@ import random
 import sys
 
 N_REINAS = 8
+MAX_POSICIONES_INVALIDAS = 28
 MAX_FITNESS = 28 	# Sin posiciones invalidas en tablero 8x8 con 8 reinas
 GENERACION_LIMITE = 100000
 NUM_TABLEROS = 1000
 BANDERA_MUTACION = True
-CRUZAR = random.uniform(0.6, 0.9)
-MUTACION = random.uniform(1/NUM_TABLEROS, 1/N_REINAS) # p_m entre 1/pop_size y 1/chromosome_length
+CRUZAR = random.uniform(0.6, 0.9) # p_c entre 0.6 y 0.9
+MUTACION = random.uniform(1/NUM_TABLEROS, 1/(N_REINAS*N_REINAS)) # p_m entre 1/pop_size y 1/chromosome_length
 
 class Tablero:
 	def __init__(self):
@@ -53,29 +54,42 @@ def mutar(hijo: Tablero):
 	"""
 	cromasomas = ''
 
-	for byte in hijo.genotipo:
-		bits = expandir_byte(byte)
-
-		for bit in bits:
-			cromasomas += bit if random.random() > MUTACION else bit_flip(bit)
+	bstr_genotipo = bytearray_to_bstr(hijo.genotipo)
+	
+	for bit in bstr_genotipo:
+		cromasomas += bit if random.random() > MUTACION else bit_flip(bit)
 
 	genotipo: bytearray = bstr_to_bytearray(cromasomas)
 	
 	hijo.genotipo = genotipo
 
+def bytearray_to_bstr(bytearray):
+	return ''.join([expandir_byte(byte) for byte in bytearray])
+
 def cruzamiento_1_punto(padre_1: Tablero, padre_2: Tablero):
 	globals()
-	num_bytes = N_REINAS
-	corte = random.randrange(num_bytes)
+	num_bits = N_REINAS * N_REINAS
+	corte = random.randrange(num_bits)
+
+	bstr_genotipo_p1 = bytearray_to_bstr(padre_1.genotipo)
+	bstr_genotipo_p2 = bytearray_to_bstr(padre_2.genotipo)
+
+	hijo_1_genotipo = bstr_to_bytearray(
+		bstr_genotipo_p1[0:corte] +
+		bstr_genotipo_p2[corte:]
+	)
+
+	hijo_2_genotipo = bstr_to_bytearray(
+		bstr_genotipo_p2[0:corte] +
+		bstr_genotipo_p1[corte:]
+	)
 
 	hijo_1 = Tablero()
-	hijo_1.genotipo.extend(padre_1.genotipo[0:corte])
-	hijo_1.genotipo.extend(padre_2.genotipo[corte:])
+	hijo_1.set_genotipo(hijo_1_genotipo)
 	hijo_1.set_fitness(fitness(hijo_1.genotipo))
 
 	hijo_2 = Tablero()
-	hijo_2.genotipo.extend(padre_2.genotipo[0:corte])
-	hijo_2.genotipo.extend(padre_1.genotipo[corte:])
+	hijo_2.set_genotipo(hijo_2_genotipo)
 	hijo_2.set_fitness(fitness(hijo_2.genotipo))
 
 	return hijo_1, hijo_2
@@ -101,7 +115,7 @@ def generar_nueva_generacion(generacion: int, populacion: list):
 
 	print(f'Ejecutando generacion genetica: {generacion}')
 
-	for _ in range(NUM_TABLEROS):
+	for _ in range(int(NUM_TABLEROS/2)):
 		padre_1, padre_2 = seleccionar_padres(populacion)
 		# print("Padres generados : ", padre_1, padre_2)
 
@@ -125,36 +139,39 @@ def ataques_de_reinas(genotipo: bytearray):
 	# [1,1,1,2,2,2] - [1,2] => 4 clashes
 	filas_con_reina = []
 	columnas_con_reina = []
-	b = ''
+	reinas_en_tablero = 0
+
 	for fila in range(N_REINAS):
 		bits_en_fila = expandir_byte(genotipo[fila])
-		b += bits_en_fila + ' '
+		
 		for columna in range(N_REINAS):
 			if(bits_en_fila[columna] == '1'):
 				filas_con_reina.append(fila)
 				columnas_con_reina.append(columna)
+				reinas_en_tablero += 1
 
-	if b.count('1') != 8:
-		print(b)
+	if reinas_en_tablero != N_REINAS: # Si hay mutacion en genotipo
+		return MAX_POSICIONES_INVALIDAS
 
 	ataques_horizontales = len(filas_con_reina) - len(set(filas_con_reina))
 	ataques_verticales = len(columnas_con_reina) - len(set(columnas_con_reina))
 	
 	ataques_diagonales = 0
 
-	#print(genotipo)
-	for i in range(N_REINAS):
+	for i in range(N_REINAS-1): # n-1 porque ultima posicion no se compara con otra posicion
 		fila_1 = filas_con_reina[i]
 		columna_1 = columnas_con_reina[i]
-		
-		if i != N_REINAS-1:
-			for j in range(i+1, N_REINAS):
-				fila_2 = filas_con_reina[j]
-				columna_2 = columnas_con_reina[j]
+	
+		for j in range(i+1, N_REINAS):
+			fila_2 = filas_con_reina[j]
+			columna_2 = columnas_con_reina[j]
 
-				if fila_2 - fila_1 == abs(columna_1 - columna_2):
-					ataques_diagonales += 1
-
+			if fila_2 - fila_1 == abs(columna_1 - columna_2):
+				ataques_diagonales += 1
+	print(filas_con_reina)
+	print(columnas_con_reina)
+	if ataques_horizontales + ataques_verticales + ataques_diagonales == 0:
+		print('max')
 	return ataques_horizontales + ataques_verticales + ataques_diagonales
 
 
@@ -201,14 +218,8 @@ def generar_genotipo():
 	
 	bstr_posiciones_tablero = generar_posiciones_tablero(bstr_posicion_vacia + bstr_posicion_reina)
 	
-	if bstr_posiciones_tablero.count('1') != 8:
-		print(bstr_posiciones_tablero)
 	cromosomas: bytearray = bstr_to_bytearray(bstr_posiciones_tablero)
-	b=''
-	for c in cromosomas:
-		b += expandir_byte(c) + ' '
-	if b.count('1') != 8:
-		print(b)
+	
 	return cromosomas
 
 def generar_populacion(num_tableros: int = 100):
@@ -237,7 +248,10 @@ def parar_algoritmo(populacion: list):
 	# Si fitness == 28, no hubieron reinas en posiciones invalidas
 	if MAX_FITNESS in valores_fitness: 
 		parar = True
-	elif GENERACION_LIMITE == generacion:
+	else:
+		print(max(valores_fitness))
+	
+	if GENERACION_LIMITE == generacion:
 		parar = True
 
 	return parar
@@ -245,6 +259,8 @@ def parar_algoritmo(populacion: list):
 def imprimir_soluciones(populacion: list):
 	for tablero in populacion:
 		if tablero.fitness == MAX_FITNESS: # Sin posiciones invalidos
+			print(tablero.genotipo)
+
 			for cromosoma in tablero.genotipo:
 				print(expandir_byte(cromosoma))
 
